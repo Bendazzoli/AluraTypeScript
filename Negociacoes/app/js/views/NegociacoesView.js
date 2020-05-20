e7cb2bfa223b1901fdb59a39040559b551f40c6a
Paulo Henrique Bendazzoli <pbendazzoli@ciandt.com>
2020-05-20 13:00:29 -0300

    ajustando para o uso de export e import

--- Negociacoes/app/js/views/NegociacoesView.js
+++ Negociacoes/app/js/views/NegociacoesView.js
@@ -1,6 +1,17 @@
-class NegociacoesView extends View {
-    template(model) {
-        return `
+System.register(["./View"], function (exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var View_1, NegociacoesView;
+    return {
+        setters: [
+            function (View_1_1) {
+                View_1 = View_1_1;
+            }
+        ],
+        execute: function () {
+            NegociacoesView = class NegociacoesView extends View_1.View {
+                template(model) {
+                    return `
         <table class="table table-hover table-bordered">
             <thead>
                 <tr>
@@ -13,7 +24,7 @@ class NegociacoesView extends View {
 
             <tbody>
                 ${model.paraArray().map(neg => {
-            return `
+                        return `
                         <tr>
                             <td>${neg.data.getDate()}/${(neg.data.getMonth() + 1)}/${neg.data.getFullYear()}</td>
                             <td>${neg.quantidade}</td>
@@ -21,12 +32,16 @@ class NegociacoesView extends View {
                             <td>${neg.volume}</td>
                         </tr>
                     `;
-        }).join('')}
+                    }).join('')}
             </tbody>
 
             <tfoot>
             </tfoot>
         </table>        
         `;
-    }
-}
+                }
+            };
+            exports_1("NegociacoesView", NegociacoesView);
+        }
+    };
+});
