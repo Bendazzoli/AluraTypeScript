f2856674859b62a18c2710f636254a41fa3107d7
Paulo Henrique Bendazzoli <pbendazzoli@ciandt.com>
2020-05-21 10:39:21 -0300

    Usando recurso de DOM Injector e Lazy Loading

--- Negociacoes/app/ts/controllers/NegociacaoController.ts
+++ Negociacoes/app/ts/controllers/NegociacaoController.ts
@@ -1,24 +1,26 @@
 import { NegociacoesView, MensagemView } from '../views/index';
 import { Negociacao, Negociacoes } from '../models/index';
-import {logarTempoExecucao} from '../helpers/decorators/index';
+import {domInject} from '../helpers/decorators/domInject';
 
 export class NegociacaoController {
 
+    @domInject('#data')
     private _inputData: JQuery;
+
+    @domInject('#quantidade')
     private _inputQuantidade: JQuery;
+    
+    @domInject('#valor')
     private _inputValor: JQuery;
+
     private _negociacoes = new Negociacoes();
     private _negociacoesView = new NegociacoesView('#negociacoesView');
     private _mensagemView = new MensagemView('#mensagemView');
     
     constructor() {
-        this._inputData = $('#data');
-        this._inputQuantidade = $('#quantidade');
-        this._inputValor = $('#valor');
         this._negociacoesView.update(this._negociacoes);
     }
 
-    @logarTempoExecucao()
     adiciona(event: Event) {
 
         event.preventDefault();
