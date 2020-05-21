c07b612c6b8cbac7863d318a399be5b8eff02bec
Paulo Henrique Bendazzoli <pbendazzoli@ciandt.com>
2020-05-21 13:15:35 -0300

    Implementando a importação de API Externa

--- Negociacoes/README.md
+++ Negociacoes/README.md
@@ -4,3 +4,7 @@
   - no terminal, navegar até o repo local, no caso o path Negociacoes
   - subir server a partir do comando `npm start`
   - irá abrir automaticamente o browser com a URL `http://localhost:3000/`
+
+* path `/Negociacoes/api` possui um servidor node
+  - subir servidor através do comando `npm start`
+  - ficará disponível em `http://localhost:8080/`
--- Negociacoes/app/ts/controllers/NegociacaoController.ts
+++ Negociacoes/app/ts/controllers/NegociacaoController.ts
@@ -45,6 +45,24 @@ export class NegociacaoController {
         this._mensagemView.update('Negociação adicionada com sucesso!');
     }
 
+    importaDados(){
+        function isOK(res: Response){
+            if(res.ok){
+                return res;
+            }else{
+                throw new Error(res.statusText);
+            }
+        }
+        fetch('http://localhost:8080/dados')
+            .then(res => isOK(res))
+            .then(res => res.json())
+            .then((dados: any[]) => {dados.map(dado => new Negociacao(new Date(), dado.vezes, dado.montante))
+                    .forEach(negociacao => this._negociacoes.adiciona(negociacao))
+                    this._negociacoesView.update(this._negociacoes)
+                })
+            .catch(err => console.log(err.message));
+    }
+
     private _ehDiaUtil(data: Date) {
 
         return data.getDay() != DiaDaSemana.Sabado && data.getDay() != DiaDaSemana.Domingo;
@@ -52,7 +70,6 @@ export class NegociacaoController {
 }
 
 enum DiaDaSemana {
-
     Domingo, 
     Segunda, 
     Terca, 
